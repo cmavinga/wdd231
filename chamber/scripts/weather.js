566fddcd321a42091ec5138ba9c75c2a17f5c0d4
cmavinga <cmavinga@byupathway.edu>
2026-09-25 22:16:52 +0100

    CHP14

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -3,6 +3,14 @@ currentYear.textContent = new Date().getFullYear();
 const lastModifiedParagraph = document.getElementById("lastModified");
 lastModifiedParagraph.textContent = "Last Modified: " + document.lastModified;
 
+const navButton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+
 const url = "data/members.json";
 const cards = document.querySelector(".members");
 
